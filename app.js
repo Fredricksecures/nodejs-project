535894fb1e9262f226236d2040136121537f0108
Fredricksecures <124170883+Fredricksecures@users.noreply.github.com>
2023-10-18 18:33:52 +0000

    good

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Achiever, it Works!!')
+  res.send('Hello Achiever, it Works!')
 })
 
 app.listen(port, () => {
